7e5e1139ff20e1be91720a00eab8520c1bfad70e
maddinat0r <maddin4t0r@gmail.com>
2015-01-13 17:54:50 +0100

    add improved versioning
    [ci skip]

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include "CResult.hpp"
 #include "CDispatcher.hpp"
 #include "COptions.hpp"
+#include "version.hpp"
 
 #ifdef WIN32
 	#include <WinSock2.h>
@@ -35,7 +36,7 @@ PLUGIN_EXPORT bool PLUGIN_CALL Load(void **ppData)
 	}
 	
 	
-	logprintf(" >> plugin.mysql: R40 successfully loaded.");
+	logprintf(" >> plugin.mysql: "MYSQL_VERSION" successfully loaded.");
 	return true;
 }
 
